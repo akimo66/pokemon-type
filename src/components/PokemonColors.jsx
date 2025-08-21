a7980f2233aad0f3fb7e8c1c76342cd2ed5a446a
yuu <akimopc66@gmail.com>
2025-08-21 18:22:49 +0900

    PokemonColors.jsx を更新

--- src/components/PokemonColors.jsx
+++ src/components/PokemonColors.jsx
@@ -1,22 +1,22 @@
 const PokemonColors = {
-    fire: "#FF0000",
-    water: "#0000FF",
-    grass: "#78C850",
-    electric: "#F8D030",
-    ice: "#98D8D8",
-    fighting: "#C03028",
-    poison: "#A040A0",
-    ground: "#E0C068",
-    flying: "#6890F0",
-    psychic: "#F85888",
-    bug: "#A8B820",
-    rock: "#B8A038",
-    ghost: "#705898",
-    dark: "#705848",
-    dragon: "#7038F8",
-    steel: "#B8B8D0",
-    fairy: "#EE99AC",
-    normal: "#A8A878"
-  };
+  fire: "#FF0000",
+  water: "#0000FF",
+  grass: "#78C850",
+  electric: "#F8D030",
+  ice: "#98D8D8",
+  fighting: "#C03028",
+  poison: "#A040A0",
+  ground: "#E0C068",
+  flying: "#6890F0",
+  psychic: "#F85888",
+  bug: "#A8B820",
+  rock: "#B8A038",
+  ghost: "#705898",
+  dark: "#705848",
+  dragon: "#7038F8",
+  steel: "#B8B8D0",
+  fairy: "#EE99AC",
+  normal: "#A8A878"
+};
 
-  export default PokemonColors
+export default PokemonColors
